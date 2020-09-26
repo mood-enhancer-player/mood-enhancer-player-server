3caaacacb58ef6fb61431d44b78a03c0c7228451
shubhamkhunt04 <shubhamkhunt08@gmail.com>
2020-09-26 14:30:02 +0530

    Apollo client side authentication header setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Home from "./Pages/Home";
-import Login from "./Pages/Login";
-import Register from "./Pages/Register";
-
-const App = () => {
+import { useQuery, gql } from "@apollo/client";
+const ME = gql`
+  query {
+    me {
+      id
+      email
+      username
+    }
+  }
+`;
+function App() {
+  const { data, loading, error } = useQuery(ME);
+  console.log(data, loading, error);
+  //   {
+  //   variables: { code },
+  //   skip: code.length !== 2,
+  // });
   return (
-    <Router>
-      <Route exact path="/" component={Home}></Route>
-      <Route exact path="/login" component={Login}></Route>
-      <Route exact path="/register" component={Register}></Route>
-    </Router>
+    <div className="App">
+      {error && <h1>{`You Broken It ! ${error.message}`}</h1>}
+      {!data || loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <h3>Name</h3> {data.me.username} <br />
+          <h3>Email</h3>
+          {data.me.email}
+          <br />
+          <h3>ID</h3> {data.me.id}
+        </div>
+      )}
+    </div>
   );
-};
+}
 
 export default App;
+
+// import React, { useState } from "react";
+// import "./App.css";
+
+// import { Mutation } from "react-apollo";
+// import { useQuery, gql } from "@apollo/client";
+
+// export const UPLOAD_FILE = gql`
+//   mutation uploadFile($file: Upload!) {
+//     uploadFile(file: $file) {
+//       filename
+//     }
+//   }
+// `;
+
+// function App() {
+//   return (
+//     <Mutation mutation={UPLOAD_FILE}>
+//       {(uploadFile) => (
+//         <input
+//           type="file"
+//           required
+//           onChange={({
+//             target: {
+//               validity,
+//               files: [file],
+//             },
+//           }) => validity.valid && uploadFile({ variables: { file } })}
+//         />
+//       )}
+//     </Mutation>
+//   );
+// }
+
+// export default App;
+
+// // import React from "react";
+// // import "./App.css";
+// // import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// // import Home from "./Pages/Home";
+// // import Login from "./Pages/Login";
+// // import Register from "./Pages/Register";
+
+// // const App = () => {
+// //   return (
+// //     <Router>
+// //       <Route exact path="/" component={Home}></Route>
+// //       <Route exact path="/login" component={Login}></Route>
+// //       <Route exact path="/register" component={Register}></Route>
+// //     </Router>
+// //   );
+// // };
+
+// // export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  ApolloLink,
+} from "@apollo/client";
+
+const httpLink = new HttpLink({ uri: "http://localhost:9090/" });
+
+const authLink = new ApolloLink((operation, forward) => {
+  // Retrieve the authorization token from local storage.
+  const token = localStorage.getItem("auth_token");
+
+  // Use the setContext method to set the HTTP headers.
+  operation.setContext({
+    headers: {
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  });
+
+  // Call the next link in the middleware chain.
+  return forward(operation);
+});
 
 const client = new ApolloClient({
-  uri: "http://localhost:9090",
-  cache: new InMemoryCache({}),
+  link: authLink.concat(httpLink), // Chain it with the HttpLink
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
