38dde8e8e340da94aeee49afde11d36c7ae86b4f
ShubhamKhunt <shubhamkhunt08>
2020-10-30 11:57:32 +0530

    aws s3 bucket fileupload setup complete

---
+++ common/awsSetup/s3FileUpload.js
@@ -0,0 +1,31 @@
+const AWS = require("aws-sdk");
+
+const uploadToS3 = async (stream, filename) => {
+  const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET,
+    region: "ap-south-1",
+  });
+
+  const params = {
+    ACL: "public-read",
+    Bucket: process.env.AWS_S3_BUCKET,
+    Key: filename,
+    Body: stream(),
+  };
+
+  const fileLocationOnS3 = new Promise((resolve, reject) => {
+    s3.upload(params, (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data.Location);
+    });
+  });
+
+  return {
+    SongURL: await fileLocationOnS3,
+  };
+};
+
+module.exports = uploadToS3;
--- graphql/resolvers/song.js
+++ graphql/resolvers/song.js
@@ -12,8 +12,7 @@ const checkAuth = require("../../common/utils/checkAuth");
 
 const User = require("../../models/User");
 const Song = require("../../models/Song");
-const { rejects } = require("assert");
-
+const uploadToS3 = require("../../common/awsSetup/s3FileUpload");
 // const s3 = require("./aws/s3");
 
 module.exports = {
@@ -100,33 +99,13 @@ module.exports = {
       const { id } = checkAuth(context);
       const { createReadStream, filename, mimetype, encoding } = await file;
 
-      const s3 = new AWS.S3({
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET,
-        region: "ap-south-1",
-      });
-
-      const params = {
-        ACL: "public-read",
-        Bucket: process.env.AWS_S3_BUCKET,
-        Key: filename,
-        Body: createReadStream(),
-      };
-
-      const songUrl = new Promise((resolve, reject) => {
-        s3.upload(params, (err, data) => {
-          if (err) {
-            reject(err);
-          }
-          console.log(`File uploaded succesfully ${data.Location}`);
-          resolve(data.Location);
-        });
-      });
-
+      const { SongURL } = await uploadToS3(createReadStream, filename);
+      console.log("songURL", SongURL);
       return {
-        url: await songUrl,
+        url: SongURL,
       };
 
+      // Store Into LocalDiskStorage Of Server
       // const songId = uuid.v4();
       // const stream = createReadStream();
       // const pathName = path.join(
